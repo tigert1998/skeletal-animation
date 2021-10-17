166ddd5ea4c66cc16668445394b8cdff34239cd7
tigertang <tigertang.zju@qq.com>
2021-10-17 23:22:10 +0800

    Add camera tuning

--- skeletal-animation/src/main.cpp
+++ skeletal-animation/src/main.cpp
@@ -12,6 +12,7 @@
 
 #include <GLFW/glfw3.h>
 #include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 #include <imgui.h>
 
 #include <iostream>
@@ -43,6 +44,40 @@ void FramebufferSizeCallback(GLFWwindow *window, int width, int height) {
   ::height = height;
   camera_ptr->set_width_height_ratio(static_cast<double>(width) / height);
   glViewport(0, 0, width, height);
+}
+
+void ImGuiInit() {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.DisplaySize.x = width;
+  io.DisplaySize.y = height;
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init();
+  ImGui::StyleColorsClassic();
+  auto font = io.Fonts->AddFontDefault();
+  io.Fonts->Build();
+}
+
+void ImGuiWindow() {
+  auto p = camera_ptr->position();
+  auto f = camera_ptr->front();
+  float p_arr[3] = {p.x, p.y, p.z};
+  float f_arr[3] = {f.x, f.y, f.z};
+  float alpha = camera_ptr->alpha();
+  float beta = camera_ptr->beta();
+
+  ImGui::Begin("Panel");
+  ImGui::InputFloat3("camera.position", p_arr);
+  ImGui::InputFloat3("camera.front", f_arr);
+  ImGui::InputFloat("camera.alpha", &alpha);
+  ImGui::InputFloat("camera.beta", &beta);
+  ImGui::End();
+
+  camera_ptr->set_position(vec3(p_arr[0], p_arr[1], p_arr[2]));
+  camera_ptr->set_front(vec3(f_arr[0], f_arr[1], f_arr[2]));
+  camera_ptr->set_alpha(alpha);
+  camera_ptr->set_beta(beta);
 }
 
 void Init() {
@@ -97,15 +132,7 @@ void Init() {
     }
   });
 
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  io.DisplaySize.x = width;
-  io.DisplaySize.y = height;
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui::StyleColorsClassic();
-  auto font = io.Fonts->AddFontDefault();
-  io.Fonts->Build();
+  ImGuiInit();
 }
 
 int main() {
@@ -124,13 +151,12 @@ int main() {
 
     sprite_model_ptr->Draw(0, camera_ptr, current_time);
 
-    // imgui demo
-    ImGui::NewFrame();
     ImGui_ImplGlfw_NewFrame();
-    ImGui::Begin("Demo window");
-    ImGui::Button("Hello!");
-    ImGui::End();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui::NewFrame();
+    ImGuiWindow();
     ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(window);
   }
