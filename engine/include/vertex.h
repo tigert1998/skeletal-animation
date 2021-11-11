59e8e0a0b397a0f05ba7bbf6c45f772c57e26bdb
tigertang <tigertang.zju@qq.com>
2021-11-11 21:05:27 +0800

    Fix vertex.h

--- engine/include/vertex.h
+++ engine/include/vertex.h
@@ -14,49 +14,42 @@
 
 #include "cg_exception.h"
 
-template <int MaxBonesPerVertex>
-class VertexWithBonesBase {
- public:
-  VertexWithBonesBase();
-  void AddBone(int id, float weight);
-  int NumBones();
-};
-
-template <int MaxBonesPerVertex>
-VertexWithBonesBase<MaxBonesPerVertex>::VertexWithBonesBase() {
-  std::fill(bone_ids, bone_ids + MaxBonesPerVertex, -1);
-  std::fill(bone_weights, bone_weights + MaxBonesPerVertex, 0);
-}
-
-template <int MaxBonesPerVertex>
-void VertexWithBonesBase<MaxBonesPerVertex>::AddBone(int id, float weight) {
-  int i = NumBones();
-  if (i >= MaxBonesPerVertex) throw MaxBoneExceededError();
-  bone_weights[i] = weight;
-  bone_ids[i] = id;
-}
-
-template <int MaxBonesPerVertex>
-int VertexWithBonesBase<MaxBonesPerVertex>::NumBones() {
-  for (int i = 0; i < MaxBonesPerVertex; i++)
-    if (bone_ids[i] < 0) return i;
-  return MaxBonesPerVertex;
-}
+#define IMPLEMENT_METHODS                                         \
+  int bone_ids[MaxBonesPerVertex];                                \
+  float bone_weights[MaxBonesPerVertex];                          \
+  Vertex() {                                                      \
+    std::fill(bone_ids, bone_ids + MaxBonesPerVertex, -1);        \
+    std::fill(bone_weights, bone_weights + MaxBonesPerVertex, 0); \
+  }                                                               \
+  int NumBones() {                                                \
+    for (int i = 0; i < MaxBonesPerVertex; i++)                   \
+      if (bone_ids[i] < 0) return i;                              \
+    return MaxBonesPerVertex;                                     \
+  }                                                               \
+  void AddBone(int id, float weight) {                            \
+    int i = NumBones();                                           \
+    if (i >= MaxBonesPerVertex) throw MaxBoneExceededError();     \
+    bone_weights[i] = weight;                                     \
+    bone_ids[i] = id;                                             \
+  }
 
 template <int MaxBonesPerVertex, bool HasTexCoord = true>
-class Vertex : public VertexWithBonesBase<MaxBonesPerVertex> {
+class Vertex {
  public:
   glm::vec3 position;
   glm::vec2 tex_coord;
   glm::vec3 normal;
+
+  IMPLEMENT_METHODS
 };
 
 template <int MaxBonesPerVertex>
-class Vertex<MaxBonesPerVertex, false>
-    : public VertexWithBonesBase<MaxBonesPerVertex> {
+class Vertex<MaxBonesPerVertex, false> {
  public:
   glm::vec3 position;
   glm::vec3 normal;
+
+  IMPLEMENT_METHODS
 };
 
 template <>
@@ -73,3 +66,5 @@ class Vertex<0, false> {
   glm::vec3 position;
   glm::vec3 normal;
 };
+
+#undef IMPLEMENT_METHODS
